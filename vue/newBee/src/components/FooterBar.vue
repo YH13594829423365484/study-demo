eb2b9982979bb387c6f9a7ad1251d94e97304005
YH13594829423365484 <1197315922@qq.com>
2024-01-11 17:06:24 +0800

    vuex

---
+++ vue/newBee/src/components/FooterBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <van-action-bar>
+        <van-action-bar-icon icon="chat-o" text="客服" />
+        <van-action-bar-icon icon="cart-o" text="购物车" />
+        <van-action-bar-button color="#be99ff" type="warning" text="加入购物车" />
+        <van-action-bar-button color="#7232dd" type="danger" text="立即购买" />
+    </van-action-bar>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="lcss" scoped>
+
+</style>
+<style>
+.van-button--warning{
+    background: linear-gradient(to right,#6bd8d8,);
+}
+</style>
---
+++ vue/newBee/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from 'vuex'
+
+const store = createStore({
+    state(){//全局公共数据源===data的概念
+        return{
+
+        }
+    },
+    mu
+})
+
+export default store
