de42fc542653a7ddb21f79465248105450eb5dbd
YH13594829423365484 <1197315922@qq.com>
2024-01-11 21:35:51 +0800

    vuefight

--- vue/newBee/src/components/FooterBar.vue
+++ vue/newBee/src/components/FooterBar.vue
@@ -1,23 +1,45 @@
 <template>
-    <div>
     <van-action-bar>
-        <van-action-bar-icon icon="chat-o" text="客服" />
-        <van-action-bar-icon icon="cart-o" text="购物车" />
-        <van-action-bar-button color="#be99ff" type="warning" text="加入购物车" />
-        <van-action-bar-button color="#7232dd" type="danger" text="立即购买" />
+      <van-action-bar-icon icon="chat-o" text="客服" />
+      <van-action-bar-icon
+        icon="shopping-cart-o"
+        text="购物车"
+        :badge="badgeNum"
+      />
+      <van-action-bar-button
+        color="linear-gradient :"
+        type="warning"
+        text="加入购物车"
+      />
+      <van-action-bar-button color="" type="danger" text="立即购买" />
     </van-action-bar>
-    </div>
-</template>
+  </template>
+  
+  <script setup>
+  import { useStore } from "vuex";
+  import { computed, onMounted } from "vue";
+  const store = useStore(); //仓库的实例对象
+  const badgeNum = computed(() => {
+    if (store.state.cartCount >= 1) {
+      return store.state.cartCount;
+    }
+  });
+  
+  onMounted(() => {
+      store.dispatch("setratCountAction"); //store中 actions 里面的函数都要用 dispatch 来触发
+    });
 
-<script setup>
 
-</script>
-
-<style lang="lcss" scoped>
-
-</style>
-<style>
-.van-button--warning{
-    background: linear-gradient(to right,#6bd8d8,);
-}
-</style>
+  </script>
+  
+  <style lang="less" scoped></style>
+  
+  <style>
+  .van-button--warning {
+    background: linear-gradient(to right, #6bd8d8, #1baeae);
+  }
+  .van-button--danger {
+    background: linear-gradient(to right, #0dc3c3, #098888);
+  }
+  </style>
+  
--- vue/newBee/src/components/NavBar.vue
+++ vue/newBee/src/components/NavBar.vue
@@ -2,15 +2,27 @@
   <van-tabbar v-model="active" active-color="#1baeae" route>
     <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
     <van-tabbar-item to="/category" icon="bar-chart-o">分类</van-tabbar-item>
-    <van-tabbar-item to="/cart" icon="cart-o">购物车</van-tabbar-item>
+    <van-tabbar-item to="/cart" icon="cart-o"
+    text:购物车
+    :badge="badgeNum" >购物车</van-tabbar-item>
     <van-tabbar-item to="/user" icon="friends-o">我的</van-tabbar-item>
   </van-tabbar>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { onMounted } from 'vue';
 import { ref } from 'vue';
+import {useStore} from 'vuex'
 const active = ref(0);
+const store=useStore()
 
+const badgeNum=computed(()=>{
+  return store.state.cartCount
+})
+onMounted(()=>{
+  store.dispatch('setCartCountAction')
+})
 </script>
 
 <style lang="less" scoped>
