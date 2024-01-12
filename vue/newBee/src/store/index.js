bcaadeabdbb68a63abcfcbc7498aca7a04b00472
YH13594829423365484 <1197315922@qq.com>
2024-01-12 16:58:35 +0800

    vuex-shopping

--- vue/newBee/src/store/index.js
+++ vue/newBee/src/store/index.js
@@ -11,18 +11,18 @@ const store = createStore({
   mutations: {
     //等同于methods 专职修改state
     //修改state的值
-    setcratCount(state, count) {
+    setCartCount(state, count) {
       state.cartCount = count;
     },
   },
   actions: {
     //等同于methods,但是actions中可以写异步的操作 调用mutations中的方法
     // setcratCount()
-    async setratCountAction(ctx) {
+    async setCartCountAction(ctx) {
       //ctx==仓库里面的实例
       const { data } = await getCart();
       console.log(data);
-      ctx.commit("setcratCount", data.length);
+      ctx.commit("setCartCount", data.length);
     },
   },
   getters: {
--- vue/newBee/src/views/Cart.vue
+++ vue/newBee/src/views/Cart.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    购物车页面
+  <div class="cart-box">
+    <SimpleHeader title="购物车" :back="false" />
+
+    <div class="cart-body">
+      <van-checkbox-group v-model="result" @change="groupChange">
+        <van-swipe-cell v-for="item in list" :key="item.goodsId">
+          <van-checkbox :name="item.cartItemId"></van-checkbox>
+          <van-card 
+          :num="item.goodsCount" 
+          :price="item.sellingPrice" 
+          :title="item.goodsName" 
+          class="goods-card"
+            :thumb="item.goodsCoverImg">
+            <template #footer>
+              <van-stepper v-model="value" min="1" max="5" @change="numChange" />
+            </template>
+          </van-card>
+          <template #right>
+            <van-button square text="删除" type="danger" class="delete-button" />
+          </template>
+        </van-swipe-cell>
+      </van-checkbox-group>
+    </div>
+
+    <!-- submit -->
+    
 
     <NavBar />
   </div>
@@ -8,8 +32,63 @@
 
 <script setup>
 import NavBar from '../components/NavBar.vue';
+import SimpleHeader from '../components/SimpleHeader.vue';
+import { getCart } from '@/api/cart.js'
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const result = ref([])
+const list = ref([])
+
+onMounted(async () => {
+  const { data } = await getCart({ pageNumber: 1 });
+  console.log(data);
+  list.value=data;
+  result.value=data.map(item => item.cartItemId);
+})
+
+const groupChange = () => {
+  console.log(result.value)
+}
+
+const numChange = () => {
+
+}
 </script>
 
 <style lang="less" scoped>
+.cart-body {
+  margin: 16px 0 100px 0;
+  padding-left: 10px;
+}
+</style>
+<style>
+.van-swipe-cell__wrapper {
+  display: flex;
+}
 
+.van-card.goods-card {
+  width: 100%;
+  background-color: #fff;
+}
+
+.van-checkbox {
+  width: 22px;
+}
+
+.delete-button {
+  height: 100%;
+}
+
+.van-card__footer {
+  position: absolute;
+  right: 16px;
+  bottom: 8px;
+}
+
+.van-card__num {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
 </style>
