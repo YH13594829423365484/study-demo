24cd70f39f255a29ab9b958c16e76c67bc098a54
YH13594829423365484 <1197315922@qq.com>
2024-01-18 21:31:19 +0800

    uni-app

---
+++ codespace/uni-app/base/base/pages/about/about.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		about page
+	</view>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+	onMounted(()=>{
+		const req=uni.request({
+			url:'https://mock.mengxuegu.com/mock/65a9154ec4cd67421b34c8b9/movie/movieList',
+			method:'GET'
+		}).then(res => {
+			console.log(res);
+		}
+	})
+</script>
+
+<style lang="scss">
+
+</style>
